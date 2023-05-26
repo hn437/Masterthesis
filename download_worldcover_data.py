3fb37b2f097cf6119d57ecf397916cb40526bcaa
hn437 <Danner@stud.uni-heidelberg.de>
2023-05-26 13:54:58 +0200

    reworked download_worldcover_data.py. Now all rasters touched by features are downloaded, clipped to the features extent, merged if a feature overlays multiple rasters and moved to the correct dirs for maps and quality_rasters respectively

--- download_worldcover_data.py
+++ download_worldcover_data.py
@@ -4,14 +4,18 @@ import shutil
 import sys
 
 import geopandas as gpd
+import rasterio as rio
+import shapely
+from rasterio.mask import mask
+from rasterio.merge import merge
 from terracatalogueclient import Catalogue
 
 from config import INFILES, INPUTDIR, PASSWORD, TERRADIR, USERNAME
 
 
-def import_geodata(inputdir: str, infile: str) -> gpd.GeoDataFrame:
+def import_geodata(input_dir: str, infile: str) -> gpd.GeoDataFrame:
     if infile is not None:
-        path_to_file = pathlib.Path(inputdir, infile)
+        path_to_file = pathlib.Path(input_dir, infile)
         df = gpd.read_file(path_to_file)
 
         if df.crs != 4326:
@@ -31,16 +35,17 @@ def download_terrascope_data(directory: pathlib.Path) -> None:
             if filename.endswith(".geojson"):
                 INFILES.append(filename)
 
-    file_counter = 1
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    file_counter = 0
     for infile in INFILES:
-        print(f"\nWorking on Inputfile {file_counter} of {len(INFILES)}")
+        print(f"\nWorking on Inputfile {file_counter +1} of {len(INFILES)}")
         # import geodata of extent to be imported from WorldCover
         gdf = import_geodata(INPUTDIR, infile)
 
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-
         for index in gdf.index:
+            print(f"\tWorking on feature {index+1} of {len(gdf)}")
             feature = gdf.loc[[index]]
             geom = feature.geometry[index]
 
@@ -52,23 +57,27 @@ def download_terrascope_data(directory: pathlib.Path) -> None:
             )
 
             # download the products to the given directory
-            sys.stdout.write(
-                f"\r\tDownloading WorldCover data for feature {index+1} of {len(gdf)}"
-            )
+            print(f"\t\tStarted download of WorldCover data.")
             catalogue.download_products(products_2020, directory, force=True)
             catalogue.download_products(products_2021, directory, force=True)
+            print(f"\t\tFinished download of WorldCover data.")
+
+            clean_terradata(directory, file_counter, index, geom)
 
         file_counter += 1
 
-    print(
-        f"\n\nFinished downloading WorldCover Data for all Files and Features to "
-        f"directory {directory}\n\n"
-    )
+    # remove scratch dir
+    shutil.rmtree(directory)
+
+    print(f"\n\nFinished downloading WorldCover Data for all Files and Features.")
 
 
-def clean_terradata(scratch_dir: pathlib.Path) -> None:
-    print(f"Extracting WorldCover Mapdata and Quality Data")
-
+def clean_terradata(
+    scratch_dir: pathlib.Path,
+    file_no,
+    feature_id: int,
+    feature_geom: shapely.geometry.polygon.Polygon,
+) -> None:
     maps_dir = TERRADIR + "Maps/"
     quality_dir = TERRADIR + "Quality/"
     if not os.path.exists(maps_dir):
@@ -76,27 +85,105 @@ def clean_terradata(scratch_dir: pathlib.Path) -> None:
     if not os.path.exists(quality_dir):
         os.makedirs(quality_dir)
 
-    for file in scratch_dir.rglob("*_Map.tif"):
-        shutil.move(file, maps_dir + file.name)
+    # clip all raster
+    print(f"\t\tClipping WorldCover Data to Feature Extent.")
+    clip_all_downloads(scratch_dir, feature_geom)
 
-    for file in scratch_dir.rglob("*_InputQuality.tif"):
-        shutil.move(file, quality_dir + file.name)
+    # check if AoI overlays multiple WorldCover Tiles
+    if len(list(scratch_dir.rglob("*2020*_Map.tif"))) > 1:
+        print(f"\t\tMerging WorldCover Data and saving it")
+        # make lists of files olding Maps or Data Quality per year
+        maps_20 = list(scratch_dir.rglob("*2020*_Map.tif"))
+        maps_21 = list(scratch_dir.rglob("*2021*_Map.tif"))
+        quality_20 = list(scratch_dir.rglob("*2020*_InputQuality.tif"))
+        quality_21 = list(scratch_dir.rglob("*2021*_InputQuality.tif"))
 
-    shutil.rmtree(scratch_dir)
+        # Merge tiles per year and type
+        merge_tiles(maps_20, maps_dir, "Map", file_no, feature_id)
+        merge_tiles(quality_20, quality_dir, "InputQuality", 1, feature_id)
+        merge_tiles(maps_21, maps_dir, "Map", file_no, feature_id)
+        merge_tiles(quality_21, quality_dir, "InputQuality", 1, feature_id)
 
-    print(f"Finished! All Mapdata located in {maps_dir}, Qualitydata in {quality_dir}")
+    else:
+        print(f"\t\tSaving WorldCover Data.")
+        # AoI within a single raster tile -> no merging, but renaming and moving to dir
+        for file in scratch_dir.rglob("*_Map.tif"):
+            new_name = f"ESA_WorldCover_10m_{str(file.stem)[19:29]}f{file_no:02}id{feature_id:02}_Map.tif"
+            os.rename(file, maps_dir + new_name)
+
+        for file in scratch_dir.rglob("*_InputQuality.tif"):
+            new_name = f"ESA_WorldCover_10m_{str(file.stem)[19:29]}f{file_no:02}id{feature_id:02}_InputQuality.tif"
+            os.rename(file, quality_dir + new_name)
+
+
+def clip_all_downloads(
+    scratch_dir: pathlib.Path, feature_geom: shapely.geometry.polygon.Polygon
+) -> None:
+    for tile in list(scratch_dir.rglob("*.tif")):
+        with rio.open(tile) as src:
+            out_image, out_transform = mask(src, [feature_geom], crop=True)
+            out_meta = src.meta.copy()  # copy the metadata of the source DEM
+
+        out_meta.update(
+            {
+                "driver": "Gtiff",
+                "height": out_image.shape[1],  # height starts with shape[1]
+                "width": out_image.shape[2],  # width starts with shape[2]
+                "transform": out_transform,
+            }
+        )
+
+        out_name = str(tile.stem)[:4] + "FileMasked" + str(tile.name)[14:]
+        out_path = pathlib.Path(scratch_dir / out_name)
+        with rio.open(out_path, "w", **out_meta) as dst:
+            dst.write(out_image)
+        os.remove(tile)
+
+    for file in os.listdir(scratch_dir):
+        d = os.path.join(scratch_dir, file)
+        if os.path.isdir(d):
+            os.rmdir(d)
+
+
+def merge_tiles(
+    tiles: list, dir_path: str, tiletype: str, file_no: int, feature_id: int
+) -> None:
+    raster_to_mosiac = []
+    for tile in tiles:
+        raster = rio.open(tile)
+        raster_to_mosiac.append(raster)
+
+    mosaic, output = merge(raster_to_mosiac)
+
+    output_meta = raster.meta.copy()
+    output_meta.update(
+        {
+            "driver": "GTiff",
+            "height": mosaic.shape[1],
+            "width": mosaic.shape[2],
+            "transform": output,
+        }
+    )
+    output_path = (
+        dir_path
+        + f"ESA_WorldCover_10m_{str(tile.stem)[19:23]}_v100_f{file_no:02}id{feature_id:02}_{tiletype}.tif"
+    )
+    with rio.open(output_path, "w", **output_meta) as m:
+        m.write(mosaic)
+
+    for file in tiles:
+        os.remove(file)
 
 
 def main():
     path_terradir_scratch = pathlib.Path(TERRADIR + "scratch/")
 
     download_terrascope_data(path_terradir_scratch)
-    clean_terradata(path_terradir_scratch)
 
-    #TODO:
-    # Improve Logging
-    # add Docstrings
-    # add Comments
+    # TODO:
+    #  Improve Logging
+    #  add Docstrings
+    #  add Comments
 
 
 if __name__ == "__main__":
