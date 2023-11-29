00ccd5015208b3da3f772ad1f71defd97fc59d9c
hn437 <Danner@stud.uni-heidelberg.de>
2023-11-29 13:40:42 +0100

    added run of calculate correlation to run full analysis script

--- run_full_analysis.py
+++ run_full_analysis.py
@@ -4,23 +4,29 @@ import logging.config
 
 import yaml
 
+import download_worldcover_data
 import compare_rasters
 import download_osm_data
-import download_worldcover_data
+import calculate_correlation
 from config import LOGGCONFIG
 
 
 async def main():
-    logging.info(f"Running Script 1: Downloading WC Data")
+    logging.info("Running Script 1: Downloading WC Data")
     download_worldcover_data.main()
-    logging.info(f"Finished Script 1")
-    logging.info(f"Running Script 2: Downloading OSM Data")
+    logging.info("Finished Script 1")
+    logging.info("Running Script 2: Downloading OSM Data")
     await download_osm_data.main()
-    logging.info(f"Finished Script 2")
-    logging.info(f"Running Script 3: Comparing Data")
-    compare_rasters.main(compare_wc=True, compare_wc_osm=True, compare_osm=True)
-    logging.info(f"Finished Script 3")
-    logging.info(f"Analysis Finished!")
+    logging.info("Finished Script 2")
+    logging.info("Running Script 3: Comparing Data")
+    compare_rasters.main(
+        compare_change=True, compare_wc=True, compare_wc_osm=True, compare_osm=True
+    )
+    logging.info("Finished Script 3")
+    logging.info("Running Script 4: Calculating Correlations")
+    calculate_correlation.main()
+    logging.info("Finished Script 4")
+    logging.info("Analysis Finished!")
 
 
 if __name__ == "__main__":
